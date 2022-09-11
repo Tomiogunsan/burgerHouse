309e43b101441e3a9c275c2a90b7b8cc13da5971
Tomi Ogunsan <tomiafolayan@gmail.com>
2022-09-11 04:21:45 -0700

    Update HeaderCartButton.js

--- src/component/Layout/HeaderCartButton.js
+++ src/component/Layout/HeaderCartButton.js
@@ -14,7 +14,7 @@ const HeaderCartButton = (props) => {
                 <span className={classes.icon}>
                     <CartIcon />
                 </span>
-                <span> Your Cart</span>
+                <span>Cart</span>
                 <span className={classes.badge}>{numberOfCartItems}</span>
             </button>
     );
